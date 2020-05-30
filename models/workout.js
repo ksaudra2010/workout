39c37036005c9626be79ac27f9aed4ad0421da0f
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-05-30 13:52:31 -0500

    Update workout.js

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  
+//   Create workoutSchema criteria
   day: {
     type: Date,
     default: Date.now
